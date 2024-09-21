fc35a168325c2c4d7c6514537ae05f05d8976c31
Innocent Mazando <innocentmazando@gmail.com>
2024-09-21 09:17:40 +0200

    Added headeings

--- app/src/components/fiber-products-list.tsx
+++ app/src/components/fiber-products-list.tsx
@@ -2,6 +2,7 @@ export default function FiberProductsList() {
   return (
     <>
       <h1 className="font-bold text-center text-3xl">Fiber Products</h1>
+      <p className="text-center my-2">Select a Fibre infrastructure provider below, browse the products available and complete a coverage search</p>
     </>
   );
 }
