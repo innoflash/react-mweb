05a378b6a11afa52352cf56ff6e545467b78041d
Innocent Mazando <innocentmazando@gmail.com>
2024-09-21 16:25:44 +0200

    Update products hook

--- app/hooks/useSelectedProducts.ts
+++ app/hooks/useSelectedProducts.ts
@@ -1,5 +1,4 @@
 import { useSelectedPromotions } from '@mweb/app/hooks/useSelectedPromotions';
-import { FibrePromotionModel } from '@mweb/app/models/fibre-product.model';
 import { AppState } from '@mweb/app/models/state/app.state.model';
 import { FILTERS } from '@mweb/app/store/filters.slice';
 import { useSelector } from 'react-redux';
@@ -7,17 +6,15 @@ import { useSelector } from 'react-redux';
 export function useSelectedProducts() {
   const selectedPromotions = useSelectedPromotions();
   const filteredProviderNames = useSelector<AppState, Array<string>>(state => state[FILTERS].providers);
-  const filterByProviderNamesFn = (promotion: FibrePromotionModel): boolean => {
+
+  return selectedPromotions.filter(promotion => {
+    //filter promotions by selected providers.
     if (!filteredProviderNames.length) {
       return true;
     }
 
     return filteredProviderNames.includes(promotion.provider);
-  }
-
-  return selectedPromotions.filter(promotion => {
-    return filterByProviderNamesFn(promotion);
-  }).map(promotion => promotion.products)
+  })
+    .map(promotion => promotion.products)
     .flat();
 }
-
