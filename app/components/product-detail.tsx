a22395a9e6db92ebef23cdc8557240a467ac05a4
Innocent Mazando <innocentmazando@gmail.com>
2024-09-21 16:45:56 +0200

    Showing campaign

--- app/components/product-detail.tsx
+++ app/components/product-detail.tsx
@@ -1,8 +1,14 @@
 import { FibreProductModel } from '@mweb/app/models/fibre-product.model';
 
-export default function ProductDetail({ product }: { product: FibreProductModel }) {
+export default function ProductDetail({ product, campaign }: { product: FibreProductModel, campaign?: string }) {
   console.log(product);
   return (
-    <div className="w-full border-2 px-4 py-3">Defensive</div>
+    <div className="w-full border-2 px-4 py-3 cursor-pointer hover:bg-gray-300">
+      <div className="flex flex-col">
+        <span>{ product.friendlyName }</span>
+        <span>{ product.parameters.find(param => param.name === 'isThrottled')?.value }</span>
+        <span>{campaign}</span>
+      </div>
+    </div>
   )
 }
--- app/components/products-list.tsx
+++ app/components/products-list.tsx
@@ -1,12 +1,19 @@
 import ProductDetail from '@mweb/app/components/product-detail';
 import { useSelectedProducts } from '@mweb/app/hooks/useSelectedProducts';
+import { Campaign } from '@mweb/app/models/fiber-campaigns.response';
+import { AppState } from '@mweb/app/models/state/app.state.model';
+import { CAMPAIGNS } from '@mweb/app/store/campaigns.slice';
+import { useSelector } from 'react-redux';
 
 export default function ProductsList() {
   const products = useSelectedProducts();
+  const selectedCampaign = useSelector<AppState, Campaign | undefined>(state => state[CAMPAIGNS].selectedCampaign);
 
   return (
     <div className="my-6 w-full grid grid-cols-2 gap-4">
-      { products.map(product => <ProductDetail key={ product.id } product={ product }/>) }
+      { products.map(product => <ProductDetail key={ product.id }
+                                               product={ product }
+                                               campaign={ selectedCampaign?.name }/>) }
     </div>
   );
 }
