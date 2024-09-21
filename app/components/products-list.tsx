44204582a075766fcf76a6c20f1db54394bbd8b4
Innocent Mazando <innocentmazando@gmail.com>
2024-09-21 19:14:23 +0200

    Showing missing products screen

--- app/components/products-list.tsx
+++ app/components/products-list.tsx
@@ -10,10 +10,17 @@ export default function ProductsList() {
   const selectedCampaign = useSelector<AppState, Campaign | undefined>(state => state[CAMPAIGNS].selectedCampaign);
 
   return (
-    <div className="my-6 w-full grid grid-cols-1 md:grid-cols-2 gap-4">
-      { products.map((product, i) => <ProductDetail key={ product.id + i }
-                                               product={ product }
-                                               campaign={ selectedCampaign?.name }/>) }
-    </div>
+    <>
+      <div className="my-6 w-full grid grid-cols-1 md:grid-cols-2 gap-4">
+        { products.map((product, i) => <ProductDetail key={ product.id + i }
+                                                      product={ product }
+                                                      campaign={ selectedCampaign?.name }/>) }
+      </div>
+      { !products.length && <div
+          className="p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300"
+          role="alert">
+          <span className="font-medium">No products found for your filter </span> for this { selectedCampaign?.name }.
+      </div> }
+    </>
   );
 }
