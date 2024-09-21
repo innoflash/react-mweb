d76cb897672b2aa8b0b8f21ec16f2a3eb2d643ed
Innocent Mazando <innocentmazando@gmail.com>
2024-09-21 09:22:43 +0200

    Added loader component

--- app/src/components/fiber-products-list.tsx
+++ app/src/components/fiber-products-list.tsx
@@ -1,8 +1,11 @@
+import Loader from '@mweb/app/src/components/loader';
+
 export default function FiberProductsList() {
   return (
     <>
       <h1 className="font-bold text-center text-3xl">Fiber Products</h1>
       <p className="text-center my-2">Select a Fibre infrastructure provider below, browse the products available and complete a coverage search</p>
+      <Loader/>
     </>
   );
 }
