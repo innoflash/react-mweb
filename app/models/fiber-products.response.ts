2d3313496ea3f0938b895e3d68746b830399f7c4
Innocent Mazando <innocentmazando@gmail.com>
2024-09-21 10:27:30 +0200

    Typed campaign

--- app/models/fiber-products.response.ts
+++ app/models/fiber-products.response.ts
@@ -1,4 +1,17 @@
 export interface FiberProductsResponse {
   links: Array<unknown>;
-  campaigns: Array<'Campaign'>;
+  campaigns: Array<Campaign>;
+}
+
+export interface Campaign {
+  links: Array<unknown>;
+  category: string;
+  code: string;
+  description: string;
+  name: string;
+  slug: string;
+  isDefaultCampaign: boolean;
+  isPrivateCampaign: boolean;
+  isStandardCampaign: boolean;
+  promocodes: Array<string>;
 }
